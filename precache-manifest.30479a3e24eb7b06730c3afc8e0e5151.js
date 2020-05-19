909126227cc012a88a37b593a83ece24e869211e
Serhii Hryhorenko <serhii.hryhorenko@dev.pro>
2020-05-19 16:58:54 +0300

    Updates

---
+++ precache-manifest.30479a3e24eb7b06730c3afc8e0e5151.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3237d9cd1c3443fa58f5cc307b80b2a",
+    "url": "/useState-useEffect/index.html"
+  },
+  {
+    "revision": "0813a3ae38a3f9c9156b",
+    "url": "/useState-useEffect/static/css/main.2fba6799.chunk.css"
+  },
+  {
+    "revision": "0e3706b3a866638d2d01",
+    "url": "/useState-useEffect/static/js/2.8cb09852.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/useState-useEffect/static/js/2.8cb09852.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0813a3ae38a3f9c9156b",
+    "url": "/useState-useEffect/static/js/main.2b917fdc.chunk.js"
+  },
+  {
+    "revision": "7bce20d56a9390ae0055",
+    "url": "/useState-useEffect/static/js/runtime-main.27fcea13.js"
+  }
+]);
